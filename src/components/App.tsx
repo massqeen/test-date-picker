230ba7bdf08f676342e393d0a1a2208d687529d2
massqeen <62159695+massqeen@users.noreply.github.com>
2022-04-19 23:21:16 +0300

    Installed required material-ui package version and added provider to the App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import 'App.css'
 
-const App = (): JSX.Element => {
-    return <div className="App">App</div>
-}
+const App = (): JSX.Element => (
+    <MuiThemeProvider>
+        <div className="App">App</div>
+    </MuiThemeProvider>
+)
 
 export default App
